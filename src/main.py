5dbbdc8214ec6b52aa5aba15a7edc426cc9db008
zakirangwala <zakirangwala@gmail.com>
2021-01-16 12:29:52 -0500

    implement twilip API => send messages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # Import Libraries
-from flask import Flask
+from flask import Flask, redirect, url_for, request 
 from flask_cors import CORS, cross_origin
 from twilio.rest import Client
 import config
@@ -16,17 +16,20 @@ auth_token = config.auth_token
 client = Client(account_sid, auth_token)
 
 # Send Message
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 @cross_origin()
 def send_message():
-    # message = client.messages.create(
-    #     from_='+18189462554',
-    #     body='ok hands',
-    #     to='+14168980216'
-    # )
-    return "<h1>jazz hands</h1>"
+    if request.method == 'POST': 
+        message = client.messages.create(
+            from_='+18189462554',
+            body='ok hands',
+            to='+14168980216'   
+        )
+        return "<h1>Success</h1>"
+    else:
+        return ""
 
 
 # Main Driver
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=True)
