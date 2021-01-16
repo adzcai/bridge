32e29c804d6ee070408e59e3505b6d27af70088e
zakirangwala <zakirangwala@gmail.com>
2021-01-16 13:58:14 -0500

    validate function + enable messaging server

--- src/main.py
+++ src/main.py
@@ -2,11 +2,9 @@
 from flask import Flask, redirect, url_for, request
 from flask_cors import CORS, cross_origin
 from twilio.rest import Client
+from commands.commands import commands
 import config
 import requests
-import parse
-
-from commands import Command
 
 # Flask app configuration
 app = Flask(__name__)
@@ -18,21 +16,36 @@ account_sid = config.account_sid
 auth_token = config.auth_token
 client = Client(account_sid, auth_token)
 
+
+# Validate Message
+def execute_command(message):
+    # read the first command token and strip the "!"
+    command = message.split(' ')[0][1:]
+
+    
+    if command not in commands:
+        client.messages.create(
+            messaging_service_sid='MG0974823613bcb3dd8987e1dc8b58eda7',
+            body='Type "!help" to get more info!',
+            to='+17809530388'
+        )
+        # send "type !help to get more information"
+        return
+
+    # otherwise, we know the command exists
+    cmd = commands[command](message)
+    cmd.exec()
+
+
 # Send Message
-
-
-def receive_message(message):
-    cmd = Command(message)
-
-
 @app.route("/", methods=['POST', 'GET'])
 @cross_origin()
 def send_message():
     if request.method == 'POST':
         message = client.messages.create(
-            from_='+18189462554',
-            body='ok hands',
-            to='+16472364001'
+            messaging_service_sid='MG0974823613bcb3dd8987e1dc8b58eda7',
+            body='Testing',
+            to='+14168980216'
         )
         return "<h1>Success</h1>"
     else:
