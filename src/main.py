cb3ab5f6b48502226301f9994e3a8e6d483d5e9f
zakirangwala <zakirangwala@gmail.com>
2021-01-16 13:23:21 -0500

    intialize messaging service

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 # Import Libraries
-from flask import Flask, redirect, url_for, request 
+from flask import Flask, redirect, url_for, request
 from flask_cors import CORS, cross_origin
 from twilio.rest import Client
 import config
 import requests
+import parse
+
+from commands import Command
 
 # Flask app configuration
 app = Flask(__name__)
@@ -16,14 +19,20 @@ auth_token = config.auth_token
 client = Client(account_sid, auth_token)
 
 # Send Message
-@app.route("/", methods = ['POST', 'GET'])
+
+
+def receive_message(message):
+    cmd = Command(message)
+
+
+@app.route("/", methods=['POST', 'GET'])
 @cross_origin()
 def send_message():
-    if request.method == 'POST': 
+    if request.method == 'POST':
         message = client.messages.create(
             from_='+18189462554',
             body='ok hands',
-            to='+14168980216'   
+            to='+16472364001'
         )
         return "<h1>Success</h1>"
     else:
@@ -32,4 +41,5 @@ def send_message():
 
 # Main Driver
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
+
