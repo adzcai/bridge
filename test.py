96c9aec3f1e877cfe0d2bd071dfe4d4244504ebc
zakirangwala <zakirangwala@gmail.com>
2021-01-16 02:53:36 -0500

    send messaging functionality

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
+# Import Libraries
 from twilio.rest import Client 
 import config
 
+# Initialize client configuration
 account_sid = config.account_sid
 auth_token = config.auth_token
 
 client = Client(account_sid, auth_token)
 
+# Send Message
 message = client.messages.create(
     from_='+18189462554',
     body ='jazz hands',
-    to='+17809530388'
+    to='+14168980216'
 )
