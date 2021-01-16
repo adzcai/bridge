62474985213e23d0d9e2a59af0bfd7289727c389
zakirangwala <zakirangwala@gmail.com>
2021-01-16 18:32:21 -0500

    command function

--- src/commands/mail.py
+++ src/commands/mail.py
@@ -1,17 +1,10 @@
-import smtplib, ssl
-from commands.base_command import Command
+# import smtplib, ssl
+# from commands.base_command import Command
 
-class Mail(Command):
-    """Command for emailing a user.
+# class Mail(Command):
+#     def exec(self):
+#     """Command for emailing a user.
 
-    Format:
-    !mail;email1@email.com, email2@email.com;subject;body
-    """
-
-    def __init__(self, raw_text):
-      super().__init__(raw_text)
-
-    def exec(self, parsed):
-        to, subject = map(str.split, self.header)
-
-        print(to, subject)
+#     Format:
+#     !mail;email1@email.com, email2@email.com;subject;body
+#     """
--- src/commands/tweet.py
+++
@@ -1,5 +0,0 @@
-from commands.base_command import Command
-
-
-class Tweet(Command):
-    pass
