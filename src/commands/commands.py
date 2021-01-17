efc003a521d5b4791dc257c205185fc64a969b13
A Heal <aydenheal.dev@gmail.com>
2021-01-16 17:54:55 -0700

    finalized mail.py file

--- src/commands/commands.py
+++ src/commands/commands.py
@@ -1,10 +1,8 @@
 from typing import no_type_check_decorator
 from commands.mail import Mail
-from commands.tweet import Tweet
 from commands.stock import Stock
 
 commands = {
   "mail": Mail,
-  "tweet": Tweet,
   "stock": Stock
 }
--- src/commands/mail.py
+++ src/commands/mail.py
@@ -1,10 +1,15 @@
-# import smtplib, ssl
-# from commands.base_command import Command
+import smtplib, ssl
+from commands.base_command import Command
+from commands.utils import send_mail
+class Mail(Command):
+    def exec(self):
+        """Command for emailing a user.
 
-# class Mail(Command):
-#     def exec(self):
-#     """Command for emailing a user.
-
-#     Format:
-#     !mail;email1@email.com, email2@email.com;subject;body
-#     """
+        Format:
+        !mail;email1@email.com, email2@email.com;subject;body
+        """
+        emails = self.args[0].split(',')
+        for email in emails:
+            send_mail(self.args[1], self.args[2], email)
+        return_text = "Sent Mail To :: " + self.args[0] +"\n" + self.args[1] + ":\n" + self.args[2]
+        return return_text
